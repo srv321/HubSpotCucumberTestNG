90dce062c128568cfab4640fe3c8a9bb8482fac8
Saurav Saha <srv321@gmail.com>
2020-04-13 00:24:38 +0530

    adding Cucumber BDD code with TestNG

---
+++ src/main/java/stepDefinitions/LoginStepDefinition.java
@@ -0,0 +1,69 @@
+package stepDefinitions;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class LoginStepDefinition {
+	
+	WebDriver driver;
+	WebDriverWait wait;
+
+	@Given("^user is already on Login Page$")
+	public void user_is_already_on_Login_Page(){
+		
+		System.setProperty("webdriver.chrome.driver",
+				"D:\\Selenium Live Training-Naveen\\drivers\\chromedriver79.0.3945.36.exe");
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.get("https://app.hubspot.com/login");
+		wait = new WebDriverWait(driver, 10);
+	
+	}
+
+	@When("^title of login page is HubSpot Login$")
+	public void title_of_login_page_is_HubSpot_Login(){
+		
+		wait.until(ExpectedConditions.titleContains("HubSpot Login"));
+		String title = driver.getTitle();
+		System.out.println(title);
+		Assert.assertEquals(title, "HubSpot Login");
+
+	}
+
+	@Then("^user enters \"([^\"]*)\" and \"([^\"]*)\"$")
+	public void user_enters_and(String username, String password) {
+		driver.findElement(By.id("username")).sendKeys(username);
+		driver.findElement(By.id("password")).sendKeys(password);
+	}
+
+	@Then("^user clicks on login button$")
+	public void user_clicks_on_login_button() {
+		driver.findElement(By.id("loginBtn")).click();
+	}
+
+	@Then("^user is on home page$")
+	public void user_is_on_home_page(){
+		wait.until(ExpectedConditions.titleContains("Reports dashboard"));
+		String title = driver.getTitle();
+		Assert.assertEquals(title, "Reports dashboard");
+	
+	}
+
+	@Then("^Close the browser$")
+	public void close_the_browser() {
+		driver.quit();
+	}
+
+}
---
+++ target/cucumber-reports/cucumber-pretty/report.js
@@ -0,0 +1,166 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("login.feature");
+formatter.feature({
+  "line": 1,
+  "name": "HubSpot Login Feature",
+  "description": "",
+  "id": "hubspot-login-feature",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "line": 3,
+  "name": "HubSpot Login Test Scenario",
+  "description": "",
+  "id": "hubspot-login-feature;hubspot-login-test-scenario",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 5,
+  "name": "user is already on Login Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "title of login page is HubSpot Login",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "user enters \"\u003cusername\u003e\" and \"\u003cpassword\u003e\"",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 8,
+  "name": "user clicks on login button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 9,
+  "name": "user is on home page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 10,
+  "name": "Close the browser",
+  "keyword": "Then "
+});
+formatter.examples({
+  "line": 13,
+  "name": "",
+  "description": "",
+  "id": "hubspot-login-feature;hubspot-login-test-scenario;",
+  "rows": [
+    {
+      "cells": [
+        "username",
+        "password"
+      ],
+      "line": 14,
+      "id": "hubspot-login-feature;hubspot-login-test-scenario;;1"
+    },
+    {
+      "cells": [
+        "srv321@gmail.com",
+        "usabI009@@#"
+      ],
+      "line": 15,
+      "id": "hubspot-login-feature;hubspot-login-test-scenario;;2"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.scenario({
+  "line": 15,
+  "name": "HubSpot Login Test Scenario",
+  "description": "",
+  "id": "hubspot-login-feature;hubspot-login-test-scenario;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 5,
+  "name": "user is already on Login Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "title of login page is HubSpot Login",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "user enters \"srv321@gmail.com\" and \"usabI009@@#\"",
+  "matchedColumns": [
+    0,
+    1
+  ],
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 8,
+  "name": "user clicks on login button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 9,
+  "name": "user is on home page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 10,
+  "name": "Close the browser",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_is_already_on_Login_Page()"
+});
+formatter.result({
+  "duration": 15245262900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.title_of_login_page_is_HubSpot_Login()"
+});
+formatter.result({
+  "duration": 7699774300,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "srv321@gmail.com",
+      "offset": 13
+    },
+    {
+      "val": "usabI009@@#",
+      "offset": 36
+    }
+  ],
+  "location": "LoginStepDefinition.user_enters_and(String,String)"
+});
+formatter.result({
+  "duration": 944148700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_clicks_on_login_button()"
+});
+formatter.result({
+  "duration": 366873500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_is_on_home_page()"
+});
+formatter.result({
+  "duration": 4794118500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.close_the_browser()"
+});
+formatter.result({
+  "duration": 1105094400,
+  "status": "passed"
+});
+});
