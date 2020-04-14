e8774a6a4c0ab40d6a100ffdd27a4f66498f8d75
Saurav Saha <srv321@gmail.com>
2020-04-14 18:57:18 +0530

    updated

--- src/main/java/MyRunner/TestRunner.java
+++ src/main/java/MyRunner/TestRunner.java
@@ -29,15 +29,15 @@ public class TestRunner {
         testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
     }
  
-//    @Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "features")
-//    public void feature(CucumberFeatureWrapper cucumberFeature) {
-//        testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
-//    }
-// 
-//    @DataProvider
-//    public Object[][] features() {
-//        return testNGCucumberRunner.provideFeatures();
-//    }
+    @Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "features")
+    public void feature(CucumberFeatureWrapper cucumberFeature) {
+        testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
+    }
+ 
+    @DataProvider
+    public Object[][] features() {
+        return testNGCucumberRunner.provideFeatures();
+    }
  
     @AfterClass(alwaysRun = true)
     public void tearDownClass() throws Exception {
